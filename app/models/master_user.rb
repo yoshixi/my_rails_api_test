1127773a0eece776d88f03a6513531a750dd351d
増渕佳輝 <yoshxj4@gmail.com>
2018-02-24 12:44:29 +0900

    devise の設定やら

--- app/models/master_user.rb
+++ app/models/master_user.rb
@@ -1,2 +1,9 @@
+
 class MasterUser < ApplicationRecord
+  devise :rememberable, :omniauthable
+
+  has_many :users, dependent: :destroy
+  def social_profile(provider)
+    users.select { |sp| sp.provider == provider.to_s }.first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
